275f915649ddafcb29c6ee074ef85f4bf4f1c66b
tatsuki.tsuchiya <tatsuki.tsuchiya@monocrea.co.jp>
2026-01-15 21:31:27 +0900

    commit

--- docs/levels.js
+++ docs/levels.js
@@ -1,56 +1,91 @@
 window.ROBO_LEVELS = [
     {
         "id": 1,
-        "name": "Level 1: First Steps",
+        "name": "Level 1: 始まりの予感",
         "grid": [
-            [0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 1, 5, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0]
+            [0, 5, 0],
+            [0, 6, 0],
+            [0, 7, 0],
+            [0, 5, 0],
+            [0, 6, 0],
+            [0, 7, 0],
+            [0, 1, 0],
         ],
-        "startPos": { "x": 1, "y": 1 },
-        "startDir": 1,
+        "startPos": { "x": 1, "y": 0 },
+        "startDir": 2,
         "configs": {
-            "f0": { "limit": 8 }, "f1": null, "f2": null,
+            "f0": { "limit": 2 }, "f1": null, "f2": null,
             "paint": { "red": false, "blue": false, "green": false },
-            "conditions": { "red": false, "blue": false, "green": false }
+            "conditions": { "red": true, "blue": true, "green": true }
         }
     },
     {
         "id": 2,
-        "name": "Level 2: The Loop",
+        "name": "Level 2: 昨日の忘れ物",
         "grid": [
-            [0, 0, 1, 1, 1, 0],
-            [0, 0, 1, 0, 1, 0],
-            [0, 0, 1, 0, 1, 0],
-            [0, 0, 1, 1, 1, 0],
-            [0, 0, 0, 0, 0, 0]
+            [0, 0, 0, 0, 0, 0, 0, 0, 6, 2],
+            [0, 0, 0, 0, 0, 0, 0, 6, 6, 0],
+            [0, 0, 0, 0, 0, 0, 6, 6, 0, 0],
+            [0, 0, 0, 0, 0, 6, 6, 0, 0, 0],
+            [0, 0, 0, 0, 6, 6, 0, 0, 0, 0],
+            [0, 0, 0, 6, 6, 0, 0, 0, 0, 0],
+            [0, 0, 6, 6, 0, 0, 0, 0, 0, 0],
+            [0, 6, 6, 0, 0, 0, 0, 0, 0, 0],
+            [6, 6, 0, 0, 0, 0, 0, 0, 0, 0],
         ],
-        "startPos": { "x": 2, "y": 0 },
+        "startPos": { "x": 0, "y": 8 },
         "startDir": 1,
         "configs": {
-            "f0": { "limit": 8 }, "f1": { "limit": 4 }, "f2": null,
+            "f0": { "limit": 5 }, "f1": null, "f2": null,
             "paint": { "red": false, "blue": false, "green": false },
-            "conditions": { "red": false, "blue": false, "green": false }
+            "conditions": { "red": true, "blue": true, "green": true }
         }
     },
     {
         "id": 3,
-        "name": "Level 3: Complex Path",
+        "name": "Level 3: 遠い日の落書き",
         "grid": [
-            [1, 1, 1, 1, 1, 1],
-            [1, 0, 1, 0, 1, 0],
-            [1, 0, 1, 0, 1, 0],
-            [1, 0, 1, 0, 1, 0],
-            [1, 1, 1, 1, 1, 0],
-            [0, 0, 0, 0, 0, 0]
+            [0, 2, 6, 0, 0, 0, 0, 0],
+            [0, 0, 6, 6, 0, 0, 0, 0],
+            [0, 0, 0, 6, 6, 0, 0, 0],
+            [0, 0, 0, 0, 6, 6, 0, 0],
+            [0, 0, 0, 0, 0, 6, 3, 0],
+            [0, 0, 0, 0, 0, 6, 3, 0],
+            [0, 0, 0, 0, 6, 6, 0, 0],
+            [0, 0, 0, 6, 6, 0, 0, 0],
+            [0, 0, 6, 6, 0, 0, 0, 0],
+            [0, 6, 6, 0, 0, 0, 0, 0],
         ],
-        "startPos": { "x": 0, "y": 0 },
+        "startPos": { "x": 1, "y": 9 },
         "startDir": 1,
         "configs": {
-            "f0": { "limit": 12 }, "f1": { "limit": 4 }, "f2": { "limit": 4 },
+            "f0": { "limit": 5 }, "f1": null, "f2": null,
             "paint": { "red": false, "blue": false, "green": false },
-            "conditions": { "red": false, "blue": false, "green": false }
+            "conditions": { "red": true, "blue": true, "green": true }
+        }
+    },
+    {
+        "id": 4,
+        "name": "Level 4: 双璧のしらべ",
+        "grid": [
+            [0, 0, 0, 0, 0, 0, 0, 3, 0],
+            [0, 0, 0, 0, 0, 0, 0, 5, 0],
+            [0, 0, 0, 0, 0, 0, 0, 5, 0],
+            [0, 0, 0, 0, 0, 0, 0, 5, 0],
+            [0, 0, 0, 0, 0, 0, 0, 5, 0],
+            [0, 7, 5, 5, 5, 5, 5, 6, 0],
+            [0, 0, 0, 0, 0, 0, 0, 5, 0],
+            [0, 0, 0, 0, 0, 0, 0, 5, 0],
+            [0, 0, 0, 0, 0, 0, 0, 5, 0],
+            [0, 0, 0, 0, 0, 0, 0, 5, 0],
+            [0, 0, 0, 0, 0, 0, 0, 3, 0],
+        ],
+        "startPos": { "x": 1, "y": 5 },
+        "startDir": 1,
+        "configs": {
+            "f0": { "limit": 6 }, "f1": null, "f2": null,
+            "paint": { "red": false, "blue": true, "green": false },
+            "conditions": { "red": true, "blue": true, "green": true }
         }
     }
 ];
