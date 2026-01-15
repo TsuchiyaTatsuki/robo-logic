f31f5f0c84259be317c190e35d193153606a1b1f
tatsuki.tsuchiya <tatsuki.tsuchiya@monocrea.co.jp>
2026-01-15 23:11:06 +0900

    feat: add 8x speed option

--- levels.js
+++ levels.js
@@ -87,5 +87,64 @@ window.ROBO_LEVELS = [
             "paint": { "red": false, "blue": true, "green": false },
             "conditions": { "red": true, "blue": true, "green": true }
         }
+    },
+    {
+        "id": 5,
+        "name": "Level 5: 逃げるんじゃねぇぞ",
+        "grid": [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 7, 7, 7, 7, 6, 7, 7, 7, 7, 1],
+            [0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ],
+        "startPos": { "x": 5, "y": 6 },
+        "startDir": 4,
+        "configs": {
+            "f0": { "limit": 4 }, "f1": { "limit": 3 }, "f2": null,
+            "paint": { "red": false, "blue": false, "green": true },
+            "conditions": { "red": true, "blue": true, "green": true }
+        }
+    },
+    {
+        "id": 6,
+        "name": "Level 6: 暁の境界線",
+        "grid": [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [5, 6, 6, 6, 5, 5, 5, 5, 5, 5, 5],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ],
+        "startPos": { "x": 10, "y": 2 },
+        "startDir": 3,
+        "configs": {
+            "f0": { "limit": 3 }, "f1": { "limit": 3 }, "f2": null,
+            "paint": { "red": false, "blue": false, "green": false },
+            "conditions": { "red": true, "blue": true, "green": true }
+        }
+    },
+    {
+        "id": 7,
+        "name": "Level 7: 誰のものでもない地図",
+        "grid": [
+            [7, 1, 5, 7, 5, 1, 5, 6, 3],
+            [5, 0, 0, 5, 0, 0, 0, 5, 0],
+            [5, 0, 0, 5, 0, 0, 0, 5, 0],
+            [1, 0, 0, 1, 0, 0, 0, 1, 0],
+            [1, 0, 0, 1, 0, 0, 0, 1, 0],
+            [5, 0, 0, 5, 0, 0, 0, 5, 0],
+            [5, 0, 0, 5, 0, 0, 0, 5, 0],
+            [7, 1, 5, 7, 5, 1, 5, 6, 0]
+        ],
+        "startPos": { "x": 3, "y": 0 },
+        "startDir": 1,
+        "configs": {
+            "f0": { "limit": 6 }, "f1": null, "f2": null,
+            "paint": { "red": false, "blue": true, "green": true },
+            "conditions": { "red": true, "blue": true, "green": true }
+        }
     }
 ];
